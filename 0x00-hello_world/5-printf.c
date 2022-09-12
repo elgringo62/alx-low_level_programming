309b8013db39a1c421e80545c0373849f1bfc6c1
Billy Munene <billymunene62@gmail.com>
2022-09-12 01:42:18 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description:Script that prints
+ *
+ * "With proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
