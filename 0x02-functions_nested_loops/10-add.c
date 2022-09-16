c1192de48648d060a5bfeb87cae6b6e1d16b2765
Billy Munene <billymunene62@gmail.com>
2022-09-16 07:07:05 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add integers
+ *
+ * @a: first argument
+ *
+ * @b: second argument
+ *
+ * Return: sum
+ *
+ */
+
+int add(int a, int b)
+{
+		return (a + b);
+}
