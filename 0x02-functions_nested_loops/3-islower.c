eda0910d25eaece056c4116d52ae3aab159c4acf
Billy Munene <billymunene62@gmail.com>
2022-09-16 06:24:59 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ *
+ * @c: character to check
+ *
+ * Return: 0 if uppercase, 1 if lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
