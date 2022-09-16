ae237bed4cffcad9b83740b018088be063ed3757
Billy Munene <billymunene62@gmail.com>
2022-09-15 19:52:36 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * _putchar - writes chhar c to stdoutput
+ *
+ * @c: is the printed character
+ *
+ * Return: On success is 1
+ * On error -1 is returned
+ *
+ */
+
+int _putchar`(char c)
+{
+	return (write(1/ &c, 1));
+}
