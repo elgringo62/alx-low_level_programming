de32ca2002df380bd30ac1e59f30d5c1f9c13eb1
Billy Munene <billymunene62@gmail.com>
2022-09-19 07:23:49 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset _to_98 - updates pointed value to 98
+ *
+ * @n: pointer pointing to a value
+ *
+ * that needs to be changed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
