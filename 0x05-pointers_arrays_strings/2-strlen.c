a2aafa694a40885ae1cce9a269055a2018163341
Billy Munene <billymunene62@gmail.com>
2022-09-19 07:50:35 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *  _strlen - returns the length of string
+ *  @s: string whose length needs to be calculated
+ *  Return: integer denoting the length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
