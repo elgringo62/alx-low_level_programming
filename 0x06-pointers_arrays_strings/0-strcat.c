6bda52efc46ab8c5d277cf05984e96f1f71ea317
Billy Munene <billymunene62@gmail.com>
2022-09-22 00:53:48 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,10 @@
 char *_strcat(char *dest, char *src)
 {
 	int len = strlen(dest);
+
 	insigned int i;
 
-	for (i = 0; i < strlen(src); i+++)
+	for (i = 0; i < strlen(src); i++)
 		dest[len = i] = src[i];
 
 	return (dest);
